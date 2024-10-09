0b9196ddfcf8916786f18d6dd93fabdb4c536f16
caio2983 <caioluna2002@gmail.com>
2024-10-08 21:17:55 -0300

    feat: updated header

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,6 +28,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="h-1/4 flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center w-1/2 gap-5">
+            <div className="flex w-full justify-center gap-12 items-center">
+              <span className="h-[34px] w-[64px] bg-[url('public/images/pumpkin.gif')]"></span>
+              <h1>Halloween Recipes</h1>
+              <span className="h-[34px] w-[64px] bg-[url('public/images/pumpkin.gif')] "></span>
+            </div>
+
+            <span className="h-px w-full bg-red-500"></span>
+          </div>
+        </header>
+
         {children}
       </body>
     </html>
