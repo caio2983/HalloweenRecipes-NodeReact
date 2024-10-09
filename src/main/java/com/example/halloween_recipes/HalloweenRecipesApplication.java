65e5d5a3a671b4f09a7db8b70165e8081da42d23
caio2983 <caioluna2002@gmail.com>
2024-10-08 21:01:23 -0300

    initial commit

---
+++ src/main/java/com/example/halloween_recipes/HalloweenRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.halloween_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloweenRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalloweenRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/halloween_recipes/HalloweenRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halloween_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloweenRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
