6dcc92a1c5ea0db696e0281d7f3ad49a13812829
caio2983 <caioluna2002@gmail.com>
2024-10-09 17:47:09 -0300

    feat: create prisma client and new category data ( "doces" )

---
+++ frontend/prisma/script.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+const dotenv = require('dotenv');
+
+
+dotenv.config()
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const categoria = await prisma.categoria.create({
+    data: {
+      nome: 'Doces',
+    },
+  });
+  console.log(categoria);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
