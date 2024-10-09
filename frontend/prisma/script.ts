cd8a4fd9777cdfb5d5cdc3fd9df318144d727d74
caio2983 <caioluna2002@gmail.com>
2024-10-09 18:36:57 -0300

    feat: create new categorie with prisma ( '' sobremesas '' )

--- frontend/prisma/script.ts
+++ frontend/prisma/script.ts
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 async function main() {
   const categoria = await prisma.categoria.create({
     data: {
-      nome: 'Bebidas',
+      nome: 'Sobremesas',
     },
   });
   console.log(categoria);
