f17fe4a43d5756d9e9ee7c43a6ad261c784556fc
caio2983 <caioluna2002@gmail.com>
2024-10-09 18:46:52 -0300

    feat: add first recipe in database

--- frontend/prisma/script.ts
+++ frontend/prisma/script.ts
@@ -13,9 +13,17 @@ async function main() {
       descricao: 'These Halloween thumbprint cookies are a great baking project for the kids - super easy to make with delightfully spooky results!',
       cookingTime: '10 to 30 mins',
       serves: 'Makes 12–14',
-      font: 'Autor da Receita',
-      ingredientes: 'Farinha, açúcar, chocolate em pó, ovos, leite',
-      categoriaId: 1, 
+      font: ['https://www.bbc.co.uk/food/recipes/spooky_skeleton_cookies_58229','https://www.bbc.co.uk/food/chefs/liberty_mendez'],
+      ingredientes: [
+        "125g/4½oz unsalted butter, softened",
+        "75g/2¾oz caster sugar",
+        "1 large free-range egg yolk",
+        "½ tsp vanilla bean paste",
+        "200g/7oz plain flour",
+        "60g/2¼oz strawberry jam",
+        "40g/1½oz dark chocolate, melted"
+      ],
+      categoriaId: 5, 
     },
   });
   console.log(receita);
