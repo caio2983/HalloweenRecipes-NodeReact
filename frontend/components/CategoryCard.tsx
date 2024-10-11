ee7cdd31d79b5ddc2d4e0b43d42b49358ba9daef
caio2983 <caioluna2002@gmail.com>
2024-10-11 17:45:40 -0300

    refactor: change carousel and categorycard to function components

--- frontend/components/CategoryCard.tsx
+++ frontend/components/CategoryCard.tsx
@@ -5,21 +5,20 @@ import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 
-class CategoryCard extends Component {
-  render() {
-    return (
-      <div className="w-[45vw] h-[70vh] border-2 border-darkPurple">
-        <div className="h-[70%] w-full relative">
-          <Image
-            src="/static/spookycookies.jpg"
-            alt="Spooky Cookies"
-            fill
-            objectFit="cover"
-          />
-        </div>
+export default function CategoryCard() {
+  return (
+    <div className="w-[45vw] h-[70vh]  flex flex-col items-center">
+      <div className="h-[70%] w-full relative mb-[16px]">
+        <Image
+          src="/static/spookycookies.jpg"
+          alt="Spooky Cookies"
+          fill
+          objectFit="cover"
+        />
       </div>
-    );
-  }
-}
 
-export default CategoryCard;
+      <h2 className="text-darkPurple font-body bold">Sweets</h2>
+      <p className="text-pumpkinOrange font-body ">Description</p>
+    </div>
+  );
+}
--- frontend/components/carousel.tsx
+++ frontend/components/carousel.tsx
@@ -5,58 +5,54 @@ import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 
-class CarouselComponent extends Component {
-  render() {
-    return (
-      <Carousel className="border-b-2 border-darkPurple ">
-        <Carousel.Item>
-          <div className="h-[80vh] ">
-            <Image
-              src="/static/spookycookies.jpg"
-              alt="Spooky Cookies"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+export default function CarouselComponent() {
+  return (
+    <Carousel className="border-b-2 border-darkPurple ">
+      <Carousel.Item>
+        <div className="h-[80vh] ">
+          <Image
+            src="/static/spookycookies.jpg"
+            alt="Spooky Cookies"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
 
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="h-[80vh]">
-            <Image
-              src="/static/spookycookies.jpg"
-              alt="Spooky Cookies"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="h-[80vh]">
-            <Image
-              src="/static/spookycookies.jpg"
-              alt="Spooky Cookies"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
-  }
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="h-[80vh]">
+          <Image
+            src="/static/spookycookies.jpg"
+            alt="Spooky Cookies"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="h-[80vh]">
+          <Image
+            src="/static/spookycookies.jpg"
+            alt="Spooky Cookies"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
-
-export default CarouselComponent;
