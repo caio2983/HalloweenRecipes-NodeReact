6be10f715957dee99a6d592c65a721a7421424a0
caio2983 <caioluna2002@gmail.com>
2024-10-14 12:34:00 -0300

    feat: add routing in carousel

--- frontend/components/carousel.tsx
+++ frontend/components/carousel.tsx
@@ -4,6 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function CarouselComponent() {
   const [data, setData] = useState([]);
@@ -36,33 +37,39 @@ export default function CarouselComponent() {
       {data.slice(1, 6).map((recipe: any) => {
         return (
           <Carousel.Item>
-            <div className="h-[80vh] ">
-              <Image
-                src={recipe.imageLink}
-                alt="Spooky Cookies"
-                layout="fill"
-                objectFit="cover"
-                className="opacidade"
-              />
-            </div>
+            <Link
+              href={`/recipePage/${recipe.titulo}/${recipe.id}`}
+              className="no-underline"
+            >
+              <div className="h-[80vh] ">
+                <Image
+                  src={recipe.imageLink}
+                  alt="Spooky Cookies"
+                  layout="fill"
+                  objectFit="cover"
+                  className="opacidade"
+                />
+              </div>
 
-            <Carousel.Caption>
-              <h3 className="font-body font-bold m-0 ">{recipe.titulo}</h3>
-              <p>
-                <a
-                  className="no-underline text-zinc-300 font-body"
-                  href={recipe.font[0]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Source
-                </a>
-              </p>
-              <p className="font-body font-bold text-pumpkinOrange">
-                {recipe.descricao}
-              </p>
-            </Carousel.Caption>
+              <Carousel.Caption>
+                <h3 className="font-body font-bold m-0 ">{recipe.titulo}</h3>
+                <p>
+                  <a
+                    className="no-underline text-zinc-300 font-body"
+                    href={recipe.font[0]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Source
+                  </a>
+                </p>
+                <p className="font-body font-bold text-pumpkinOrange">
+                  {recipe.descricao}
+                </p>
+              </Carousel.Caption>
+            </Link>
           </Carousel.Item>
+          //{" "}
         );
       })}
     </Carousel>
